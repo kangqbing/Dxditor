66ac21be6ce55e21224c7c149b897fce6becf062
race <kimi2009@vip.qq.com>
2018-05-31 12:59:01 +0800

    郑州局只留坠坨2

--- app/src/main/java/itor/topnetwork/com/dxditor/utils/Constants.java
+++ app/src/main/java/itor/topnetwork/com/dxditor/utils/Constants.java
@@ -14,24 +14,26 @@ public class Constants {
     //public static String ORIGINALIP = "36.110.98.226:5680";
 
     public static String HTTP = "http://";
+    public static String WEBAPPNAME = "/iotr";
 
     //public static String IP = "http://10.1.4.132:8080/iotr";
+
     //首页设备信息
-    public static String getAppDeviceStausCount =  "/iotr_product/iotrApp/iotr/iotrApp/iotrAppDataDevice!getAppDeviceStausCount.action";
+    public static String getAppDeviceStausCount = WEBAPPNAME+ "/iotrApp/iotr/iotrApp/iotrAppDataDevice!getAppDeviceStausCount.action";
     //获取设备告警信息占比
-    public static String getAppDeviceAlarmProportion =  "/iotr_product/iotrApp/iotr/iotrApp/iotrAppDataDevice!getAppDeviceAlarmProportion.action";
+    public static String getAppDeviceAlarmProportion =  WEBAPPNAME+"/iotrApp/iotr/iotrApp/iotrAppDataDevice!getAppDeviceAlarmProportion.action";
     //获取铁路线中不同设备数量,铁路线告警数
-    public static String getAppDeviceCount = "/iotr_product/iotrApp/iotr/iotrApp/iotrAppDataDevice!getAppDeviceCount.action";
+    public static String getAppDeviceCount = WEBAPPNAME+"/iotrApp/iotr/iotrApp/iotrAppDataDevice!getAppDeviceCount.action";
     //桥梁趋势
-    public static String getAppBridgeMonthDisplacementInfo =  "/iotr_product/iotrApp/iotr/iotrApp/iotrAppDataBridge!getAppBridgeMonthDisplacementInfo.action";
+    public static String getAppBridgeMonthDisplacementInfo =  WEBAPPNAME+"/iotrApp/iotr/iotrApp/iotrAppDataBridge!getAppBridgeMonthDisplacementInfo.action";
     //桥梁告警
-    public static String getAppBridgeNewOneAlarm =  "/iotr_product/iotrApp/iotrAppDataBridge!getAppBridgeNewOneAlarm.action";
+    public static String getAppBridgeNewOneAlarm =  WEBAPPNAME+"/iotrApp/iotrAppDataBridge!getAppBridgeNewOneAlarm.action";
     //坠坨现场测试
-    public static String getAppWeightsTrendInfo =  "/iotr_product/iotrApp/iotrAppDataWeights!getAppWeightsTrendInfo.action";
+    public static String getAppWeightsTrendInfo =  WEBAPPNAME+"/iotrApp/iotrAppDataWeights!getAppWeightsTrendInfo.action";
     //声屏障监测指标
-    public static String getAppSoundBarrierNewOneAlarm =  "/iotr_product/iotrApp/iotrAppDataSoundBarrier!getAppSoundBarrierNewOneAlarm.action";
+    public static String getAppSoundBarrierNewOneAlarm =  WEBAPPNAME+"/iotrApp/iotrAppDataSoundBarrier!getAppSoundBarrierNewOneAlarm.action";
     //声屏障趋势图
-    public static String getAppSoundBarrierInfo = "/iotr_product/iotrApp/iotrAppDataSoundBarrier!getAppSoundBarrierInfo.action";
+    public static String getAppSoundBarrierInfo = WEBAPPNAME+"/iotrApp/iotrAppDataSoundBarrier!getAppSoundBarrierInfo.action";
 
     public static String BRIDGECODE = "ZX0001";
 
